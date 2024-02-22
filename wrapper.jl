d47b7ed42607d0ebc0e678654808510aead992ce
Dimitris Volteras <74971080+dimvolt@users.noreply.github.com>
2024-02-22 12:49:46 +0000

    Update wrapper.jl

--- wrapper.jl
+++ wrapper.jl
@@ -1,6 +1,7 @@
 ############################################################################################################
-########## Load required packages and raw data ########## 
+# 1. Data
 
+########## Load required packages and raw data ########## 
 using DifferentialEquations
 using Sundials
 using DelimitedFiles
@@ -28,7 +29,6 @@ uu_data, us_data, lu_data, ls_data, theta, rfp, gfp, experiment = read_all_data(
 #total counts matrix:
 total_data = uu_data + us_data + lu_data + ls_data;
 
-
 ########## Pre-process data ##########
 
 #cluster cells with respect to cell cycle position
@@ -40,28 +40,49 @@ pulse_idx = findall(x->x>=0 && x<=6, experiment)
 chase_idx = findall(x->x>=7, experiment)
 
 #empirical distributions of cells across the 5 cell cycle stages and 11 labelling conditions
-cells_per_id = [findall(x->x==e,experiment) for e in sort(unique(experiment))[2:end-1]]
-cells_per_age = [findall(x->x==τ,age) for τ in sort(unique(age))]
-cells_age_id = [[intersect(cells_age,cells_id) for cells_age in cells_per_age] for cells_id in cells_per_id]
+cells_per_id = [findall(x->x==e,experiment) for e in sort(unique(experiment))[2:end-1]];
+cells_per_age = [findall(x->x==τ,age) for τ in sort(unique(age))];
+cells_age_id = [[intersect(cells_age,cells_id) for cells_age in cells_per_age] for cells_id in cells_per_id];
 
-age_dist_pulse = length.([intersect(cells,pulse_idx) for cells in cells_per_age]) ./ length(pulse_idx)
-age_dist_chase = length.([intersect(cells,chase_idx) for cells in cells_per_age]) ./ length(chase_idx)
+n_cells_id = sum(length.(cells_per_id));
+age_id_distribution = [length.(cells) ./ n_cells_id for cells in cells_age_id]
+age_dist_pulse = length.([intersect(cells,pulse_idx) for cells in cells_per_age]) ./ length(pulse_idx);
+age_dist_chase = length.([intersect(cells,chase_idx) for cells in cells_per_age]) ./ length(chase_idx);
 
+#gene filtering
 include("gene_selection.jl")
-#gene list
-all_genes = Int64.(readdlm("data/selected_genes.txt")[:,1])
-gene_id = readdlm("data/all_gene_ids.txt")[:,1]
-all_gene_names = readdlm("data/all_gene_names.txt")[:,1]
 
-#unlabelled, labelled and total mRNA count datasets for selected genes
+#load gene list
+all_gene_ids = readdlm("data/all_gene_ids.txt")[:,1]
+all_gene_names = readdlm("data/all_gene_names.txt")[:,1]
+genes = Int64.(readdlm("data/selected_genes.txt")[:,1])
+gene_names = all_gene_names[genes];
+gene_ids = all_gene_ids[genes];
+
+#define unlabelled, labelled and total mRNA count datasets for selected genes
 u_data = uu_data[:,genes] + us_data[:,genes];
 l_data = lu_data[:,genes] + ls_data[:,genes];
 t_data = u_data + l_data;
 
-
 ########## Estimate cell-specific capture efficiencies ##########
 include("total_count_capture_efficiency.jl")
+betas = readdlm("data/capture_efficiencies.txt")[:,1];
 
-betas = estimate_betas(total_data,age,pulse_idx,chase_idx,cells_per_age)
+########## Compute summary statistics ##########
+include("data_summary_statistics.jl")
+
+pulse_data,pulse_se,chase_data,chase_se,ratio_data,ratio_se,mean_corr_data,mean_corr_se,corr_mean_data,corr_mean_se = load_summary_stats("data/summary_stats/",".txt");
+pulse_mean = get_mean_subset(pulse_data);
+pulse_mean_se = get_mean_subset(pulse_se);
+pulse_ff = get_ff_subset(pulse_data);
+pulse_ff_se = get_ff_subset(pulse_se);
+chase_mean = get_mean_subset(chase_data);
+chase_mean_se = get_mean_subset(chase_se);
+chase_ff = get_ff_subset(chase_data);
+chase_ff_se = get_ff_subset(chase_se);
+
+
+############################################################################################################
+# 2. Modelling and ABC simulations
 
 
