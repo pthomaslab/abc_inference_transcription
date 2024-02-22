7752e6a942bdc6fee766cc6ff0433ddfa6408dd8
Dimitris Volteras <74971080+dimvolt@users.noreply.github.com>
2024-02-22 16:26:33 +0000

    Update model.jl

--- model.jl
+++ model.jl
@@ -1,19 +1,3 @@
-function get_vary_map(vary_flag::Vector{Int64},n_steps::Int64)
-    local keys = []
-    local k = 1
-    for i in 1:length(vary_flag)
-        if vary_flag[i] == 0
-            push!(keys,k)
-            k += 1
-        else
-            push!(keys,[k:1:k+n_steps-1;])
-            k = k + n_steps
-        end
-    end
-    return keys
-end
-
-
 function get_rate(θ::Vector{Float64}, vary_map::Vector{Any}, scaling::Int64, t_steps::Vector{Float64}, cycle::Float64, t::Float64)::Vector{Float64}
     p = Vector{Float64}(undef,length(vary_map))
     for i in 1:length(vary_map)
@@ -33,15 +17,39 @@ function get_rate(θ::Vector{Float64}, vary_map::Vector{Any}, scaling::Int64, t_
 end
 
 
+
 function transcription_rate(cycle::Float64,t::Float64)::Float64
     return 1.0 + (mod(t,cycle)/cycle) * (<(t,cycle)) + (==(t,cycle))
 end
 
 
-
-
-
-
+function get_vary_map(vary_flag::Vector{Int64},n_steps::Int64)
+    local keys = []
+    local k = 1
+    for i in 1:length(vary_flag)
+        if vary_flag[i] == 0
+            push!(keys,k)
+            k += 1
+        else
+            push!(keys,[k:1:k+n_steps-1;])
+            k = k + n_steps
+        end
+    end
+    return keys
+end
+#=
+function get_vary_matrix(vary_map::Vector{Any},n_steps::Int64)
+    mat = Matrix{Int64}(undef,(length(vary_map),n_steps))
+    for (i,v) in enumerate(vary_map) 
+        if length(v) > 1
+            mat[i,:] = v
+        else
+            mat[i,:] = ones(n_steps) .* v
+        end
+    end
+    return mat
+end
+=#
 function labelling(lambda::Float64, texp::Float64, pulse::Float64, t::Float64)::Float64
     if t >= texp && t <= texp + pulse
         return 10^lambda
@@ -50,21 +58,22 @@ function labelling(lambda::Float64, texp::Float64, pulse::Float64, t::Float64)::
     end
 end
 
-function momentodes(dμ, μ, θ, t, cycle, texp, pulse, vary_map, t_steps, scaling)
-    p = 10 .^(get_rate(θ[1:end-1], vary_map, scaling, t_steps, cycle, t))
-    λ = labelling(θ[end], texp, pulse, t)
-    dμ[1] = p[1]*(1-μ[1]) - p[2]*μ[1]
-    dμ[2] = (1-λ)*p[3]*μ[1] - p[4]*μ[2]
-    dμ[3] = λ*p[3]*μ[1] - p[4]*μ[3]
-    dμ[4] = p[2]*μ[1] + p[1]*(1-μ[1]) - 2*(p[1]+p[2])*μ[4]
-    dμ[5] = p[3]*(1-λ)*μ[4] - (p[1]+p[2]+p[4])*μ[5]
-    dμ[6] = p[3]*λ*μ[4] - (p[1]+p[2]+p[4])*μ[6]
-    dμ[7] = p[3]*(1-λ)*μ[1] + p[4]*μ[2] + 2*p[3]*(1-λ)*μ[5] - 2*p[4]*μ[7]
-    dμ[8] = p[3]*λ*μ[5] + p[3]*(1-λ)*μ[6] - 2*p[4]*μ[8]
-    dμ[9] = p[3]*λ*μ[1] + p[4]*μ[3] + 2*p[3]*λ*μ[6] - 2*p[4]*μ[9]
+
+function f(μ::Vector{Float64},p::Vector{Float64},λ::Float64)
+    y = Vector{Float64}(undef,9)
+    y[1] = p[1]*(1-μ[1]) - p[2]*μ[1]
+    y[2] = (1-λ)*p[3]*μ[1] - p[4]*μ[2]
+    y[3] = λ*p[3]*μ[1] - p[4]*μ[3]
+    y[4] = p[2]*μ[1] + p[1]*(1-μ[1]) - 2*(p[1]+p[2])*μ[4]
+    y[5] = p[3]*(1-λ)*μ[4] - (p[1]+p[2]+p[4])*μ[5]
+    y[6] = p[3]*λ*μ[4] - (p[1]+p[2]+p[4])*μ[6]
+    y[7] = p[3]*(1-λ)*μ[1] + p[4]*μ[2] + 2*p[3]*(1-λ)*μ[5] - 2*p[4]*μ[7]
+    y[8] = p[3]*λ*μ[5] + p[3]*(1-λ)*μ[6] - 2*p[4]*μ[8]
+    y[9] = p[3]*λ*μ[1] + p[4]*μ[3] + 2*p[3]*λ*μ[6] - 2*p[4]*μ[9]
+    return y
 end
 
-
+#ode model definition conditioning on labelling periods
 function model(paramvals::Vector{Float64}, vary_map::Vector{Any}, scaling::Int64, t_steps::Vector{Float64}, iv::Vector{Float64}, tmin::Float64,
     tmax::Float64, cycle::Float64, texp::Float64, pulse::Float64)
     tspan::Tuple{Float64, Float64} = (tmin, tmax)
@@ -73,7 +82,6 @@ function model(paramvals::Vector{Float64}, vary_map::Vector{Any}, scaling::Int64
     sol = solve(odeprob, CVODE_BDF())
     return sol.u[end]
 end
-
 
 function periodic_boundary(endpoint::Vector{Float64})
     v::Vector{Float64} = copy(endpoint)
@@ -89,6 +97,7 @@ function periodic_boundary(endpoint::Vector{Float64})
     v[meanidx] = endpoint[meanidx]/2
     return v
 end
+
 
 function transient_phase(paramvals::Vector{Float64}, vary_map::Vector{Any}, scaling::Int64, t_steps::Vector{Float64}, iv::Vector{Float64}, cycle::Float64)
     #convergence threshold
@@ -119,27 +128,16 @@ function transient_phase(paramvals::Vector{Float64}, vary_map::Vector{Any}, scal
     end
     return periodic_boundary(endpoint_2)
 end
-
-
-function get_steady_state_iv(paramvals::Vector{Float64}, vary_map::Vector{Any}, scaling::Int64, n_steps::Int64, iv::Vector{Float64}, cycle::Float64)
-    t_steps::Vector{Float64} = [0.0:cycle/n_steps:cycle;]
-    #run transient dynamics to determine steady state
-    ss_iv::Vector{Float64} = transient_phase(paramvals,vary_map,scaling,t_steps,iv,cycle)
-    return ss_iv
-end 
+ 
 
 
 function trajectories(paramvals::Vector{Float64}, vary_map::Vector{Any}, scaling::Int64, t_steps::Vector{Float64}, iv::Vector{Float64}, age::Float64,
     cycle::Float64, pulse::Float64, t0::Float64, texp::Float64)
     local endpoint::Vector{Float64}
     local nsols::Int64
-    #distinguish between mean, variance & covariance indices for species of interest
-    gene_covidx::Vector{Int64} = [5,6]
     #mRNA means and covariances
     meanidx::Vector{Int64} = [2,3]
-    varidx::Vector{Int64} = [7,9]
-    covidx::Vector{Int64} = [8]    #off-diagonal covariances
-    allcovidx::Vector{Int64} = [7:9;] #all covariances
+    allcovidx::Vector{Int64} = [7:9;] 
     if age > 0 && age < cycle
         nsols = floor((age-t0)/cycle) + 1
     else
@@ -156,17 +154,7 @@ function trajectories(paramvals::Vector{Float64}, vary_map::Vector{Any}, scaling
         if τ == t0
             endpoint = model(paramvals, vary_map, scaling, t_steps, iv, τ, tf, cycle, texp, pulse)
         else
-            #set new initial conditions for next cell cycle
-            gene_coviv = endpoint[gene_covidx]
-            meaniv = endpoint[meanidx]
-            variv = endpoint[varidx]
-            coviv = endpoint[covidx]
-            #binomial partitioning
-            endpoint[gene_covidx] = gene_coviv/2
-            endpoint[covidx] = coviv/4
-            endpoint[varidx] = variv/4 + meaniv/4
-            endpoint[meanidx] = meaniv/2
-            endpoint = model(paramvals, vary_map, scaling, t_steps, endpoint, τ, tf, cycle, texp, pulse)
+            endpoint = model(paramvals, vary_map, scaling, t_steps, periodic_boundary(endpoint), τ, tf, cycle, texp, pulse)
         end
         τ = tf
     end
@@ -187,6 +175,7 @@ function syntheticdata(paramvals::Vector{Float64}, vary_map::Vector{Any}, scalin
 end
 
 
+
 function get_synthetic_data(θ::Vector{Float64}, vary_map::Vector{Any}, scaling::Int64, n_steps::Int64, iv::Vector{Float64}, agevec::Vector{Float64},
     cycle::Float64, pulse::Float64, chase::Float64, t0::Float64, downsampling::Bool, betas::Vector{Float64}, age_clusters::Vector{Int64})
     s_data::Matrix{Float64} = syntheticdata(θ, vary_map, scaling, n_steps, iv, agevec, cycle, pulse, chase, t0)
@@ -197,6 +186,12 @@ function get_synthetic_data(θ::Vector{Float64}, vary_map::Vector{Any}, scaling:
     end
 end
 
+function get_steady_state_iv(paramvals::Vector{Float64}, vary_map::Vector{Any}, scaling::Int64, n_steps::Int64, iv::Vector{Float64}, cycle::Float64)
+    t_steps::Vector{Float64} = [0.0:cycle/n_steps:cycle;]
+    #run transient dynamics to determine steady state
+    ss_iv::Vector{Float64} = transient_phase(paramvals,vary_map,scaling,t_steps,iv,cycle)
+    return ss_iv
+end 
 
 
 function get_total_synthetic_data(θ::Vector{Float64}, vary_map::Vector{Any}, scaling::Int64, n_steps::Int64, iv::Vector{Float64}, agevec::Vector{Float64},
@@ -210,33 +205,6 @@ function get_total_synthetic_data(θ::Vector{Float64}, vary_map::Vector{Any}, sc
     end
 end
 
-
-function downsample_cond_spec(data::Matrix{Float64}, betas::Vector{Float64}, cond_idx::Vector{Int64},
-                            experiment::Vector{Float64}, clusters::Vector{Int64})
-    meanidx = [1,2]
-    varidx = [3,5]
-    covidx = [4]
-    new_data = Matrix{Float64}(undef,size(data))
-    mean_betas = Vector{Float64}(undef, length(unique(clusters)))
-    m2_betas = Vector{Float64}(undef, length(unique(clusters)))  #second moments of betas
-    var_betas = Vector{Float64}(undef, length(unique(clusters)))
-    for i in 1:length(cond_idx)
-        cells = findall(x->x==cond_idx[i], experiment)
-        sub_betas = betas[cells]
-        sub_clusters = clusters[cells]
-        for c in sort(unique(sub_clusters))
-            idx = findall(x->x==c, sub_clusters)
-            mean_betas[c] = mean(sub_betas[idx])
-            m2_betas[c] = mean(sub_betas[idx].^2)
-            var_betas[c] = var(sub_betas[idx])
-        end
-        rows = (i-1)*length(unique(clusters))+1:i*length(unique(clusters))
-        new_data[rows,meanidx] = data[rows,meanidx] .* mean_betas
-        new_data[rows,varidx] = (mean_betas - m2_betas) .* data[rows,meanidx] + var_betas .* (data[rows,meanidx].^2 + data[rows,varidx]) + mean_betas.^2 .* data[rows,varidx]
-        new_data[rows,covidx] = var_betas .* data[rows,meanidx[1]] .* data[rows,meanidx[2]] + mean_betas.^2 .* data[rows,covidx]
-    end
-    return new_data
-end
 
 function downsample(data::Matrix{Float64}, betas::Vector{Float64}, clusters::Vector{Int64})
     meanidx::Vector{Int64} = [1,2]
@@ -258,7 +226,3 @@ function downsample(data::Matrix{Float64}, betas::Vector{Float64}, clusters::Vec
     return new_data
 end
 
-
-function weighted_cov(x::Vector{Float64},y::Vector{Float64}, w::Vector{Float64})
-    return sum(w .* ((x .- sum(w .* x)) .* (y .- sum(w .* y))))
-end
