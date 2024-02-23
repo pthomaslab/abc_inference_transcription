230f1fe164caa154652ea550ec8957b8b01ed9e2
Dimitris Volteras <74971080+dimvolt@users.noreply.github.com>
2024-02-23 13:56:16 +0000

    Update process_error_files.jl

--- process_error_files.jl
+++ process_error_files.jl
@@ -1,26 +1,7 @@
 using JDF
-using DelimitedFiles
-using DataFrames
 
-
-# m: modelling hypothesis index  --> {1: constant rates - scaling with size
-#                                     2: constant rates - non-scaling
-#                                     3: varying burst frequency - scaling with size
-#                                     4: varying burst size - scaling with size
-#                                     5: varying decay rate - scaling with size }
-#                                       
- 
-
-m = 1   #2, 3, 4, 5
-
-model_name = ["const","const_const","kon","alpha","gamma"][m]
-
-
-error_data = readdlm("/rds/general/user/dv19/ephemeral/errors/error_"*model_name*".txt")
-println("Read.")
-
-df = DataFrame(data,:auto)
-println("Dataframe.")
-
-JDF.save("data/large_scale_simulations/errors/error_const.jdf", df)
-println("JDF done.")
+for model_name in ["const","const_const","kon","alpha","gamma"]
+    error_data = readdlm("/rds/general/user/dv19/ephemeral/errors/error_"*model_name*".txt")
+    df = DataFrame(data,:auto)
+    JDF.save("data/large_scale_simulations/errors/error_"*model_name*".jdf", df)
+end
