a983a2270479c9a9f669234352441b564019224c
Dimitris Volteras <74971080+dimvolt@users.noreply.github.com>
2024-02-23 14:22:24 +0000

    Update accepted_particles.jl

--- accepted_particles.jl
+++ accepted_particles.jl
@@ -1,21 +1,16 @@
-f_1 = JDFFile("data/large_scale_simulations/errors/error_const.jdf")
+f_1 = JDFFile("data/large_scale_simulations/errors/error_const.jdf");
+f_2 = JDFFile("data/large_scale_simulations/errors/error_const_const.jdf");
+f_3 = JDFFile("data/large_scale_simulations/errors/error_kon.jdf");
+f_4 = JDFFile("data/large_scale_simulations/errors/error_alpha.jdf");
+f_5 = JDFFile("data/large_scale_simulations/errors/error_gamma.jdf");
 
-f_2 = JDFFile("data/large_scale_simulations/errors/error_const_const.jdf")
-
-f_3 = JDFFile("data/large_scale_simulations/errors/error_kon.jdf")
-
-f_4 = JDFFile("data/large_scale_simulations/errors/error_alpha.jdf")
-
-f_5 = JDFFile("data/large_scale_simulations/errors/error_gamma.jdf")
-
-
-model_name = ["const","const_const","kon","alpha","gamma"];
+model_names = ["const","const_const","kon","alpha","gamma"];
 
 #set acceptance threshold ε
-ε = 4.8
+ε = 4.8;
 
-for g in 1:3419
-    err_mat = Matrix{Float64}(undef,(1000000,5))
+for g in 1:length(genes)
+    err_mat = Matrix{Float64}(undef,(1000000,length(model_names)))
     err_mat[:,1] = f_1["x"*string(g)]    
     err_mat[:,2] = f_2["x"*string(g)]   
     err_mat[:,3] = f_3["x"*string(g)]    
@@ -25,18 +20,12 @@ for g in 1:3419
         v::Vector{Int64} = findall(x->x<=ε,err_mat[:,j])
         if length(v) > 0
             sort_idx::Vector{Int64} = sortperm(err_mat[v,j])
-            open("data/posteriors/particles_"*model_name[j]*".txt", "a") do io
+            open("data/posteriors/particles_"*model_names[j]*".txt", "a") do io
                 DelimitedFiles.writedlm(io, reshape(v[sort_idx],1,:))
             end
-            open("data/posteriors/errors_"*model_name[j]*".txt", "a") do io
-                DelimitedFiles.writedlm(io, reshape(sort(err_mat[v,j]),1,:))
-            end
         else
-            open("data/posteriors/particles_"*model_name[j]*".txt", "a") do io
+            open("data/posteriors/particles_"*model_names[j]*".txt", "a") do io
                 DelimitedFiles.writedlm(io, [0])
-            end
-            open("data/posteriors/errors_"*model_name[j]*".txt", "a") do io
-                DelimitedFiles.writedlm(io, [10.0])
             end
         end
     end
