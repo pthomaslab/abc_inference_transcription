69303994afe1cac61720240b7a4abbdc6a31ade4
Dimitris Volteras <74971080+dimvolt@users.noreply.github.com>
2024-02-27 17:42:55 +0000

    Update constant_genes.jl

--- scripts/constant_genes.jl
+++ scripts/constant_genes.jl
@@ -29,3 +29,28 @@ p = heatmap(norm_sets, xticks = ([1:5;],cycle_label),title = "observed mean expr
         color = :viridis, clims = (-1.79,1.79), right_margin = 5mm, size = (450,250), dpi=200);
 savefig(p,"data/paper_figures/figure_4/observed_mean_expression.pdf") 
 
+
+# compare properties of constant scaling vs non-scaling genes
+mat_1 = mean_data[sel_genes[1],:] ./ mean_data[sel_genes[1],1];
+mat_2 = mean_data[sel_genes[2],:] ./ mean_data[sel_genes[2],1];
+col = [:skyblue4, :lightcyan4];
+h0 = boxplot(mat_1[:,end], linewidth = 1, color = col[1], label = false, legendfontsize = 11, xtickfontsize = 11, legend = :topright, 
+            dpi = 200, size = (400,300), xticks = ([1,2],["scaling genes","non-scaling genes"]), ylabel = "mean expression fold change", outliers = false);
+h0 = boxplot!(mat_2[:,end], linewidth = 1, color = col[2], label = false, outliers = false);
+savefig(h0,"data/paper_figures/figure_4/fold_change_const_genes.svg")
+
+
+mat_1 = mean_data[sel_genes[1],:];
+mat_2 = mean_data[sel_genes[2],:];
+der_1 = [mean(diff(mat_1[i,:])) for i in 1:size(mat_1)[1]];
+der_2 = [mean(diff(mat_2[i,:])) for i in 1:size(mat_2)[1]];
+p = boxplot(der_1, linewidth = 1, color = col[1], label = false, legendfontsize = 11, legend = :topright, size = (400,300),
+            dpi = 200, xtickfontsize = 11,xticks = ([1,2],["scaling genes","non-scaling genes"]), ylabel = "mean rate of change",outliers=false);
+p = boxplot!(der_2, linewidth = 1, color = col[2], label = false,outliers=false);
+savefig(p, "data/paper_figures/figure_4/mean_deriv_const_genes.svg")
+
+
+p = boxplot(10 .^map_sets[1][:,4], linewidth = 1, color = col[1], label = false, legendfontsize = 11,ylabel = "decay rate",xticks = ([1,2],["scaling genes","non-scaling genes"]),
+     legend = :topright, dpi = 200, size = (400,300), xtickfontsize = 11, outliers = false);
+p = boxplot!(10 .^map_sets[2][:,4], linewidth = 1,  color = col[2], label = false, outliers = false);
+savefig(p, "data/paper_figures/figure_4/decay_rate.svg")
